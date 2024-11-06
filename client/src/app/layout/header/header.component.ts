668d128e1c1539fa5cd724415f5fb2578a7b86c3
Emral Yücel <e.eyucel@hotmail.com>
2024-11-06 16:09:42 +0300

    Section 11 Client Error Handling

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatIconModule} from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { BusyService } from '../../core/services/busy.service';
 
 
 @Component({
@@ -15,14 +17,43 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatIconModule,
     CommonModule,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatProgressBar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  busyService = inject(BusyService);
+
   isMenuOpen = false;
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  isHeaderVisible = true;
+  private previousScrollY = window.scrollY;
+  private ticking = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollY = window.scrollY;
+
+    // Aşağı kaydırırken header'ı gizle, yukarı kaydırırken göster
+    if (currentScrollY > this.previousScrollY) {
+      this.isHeaderVisible = false;
+    } else {
+      this.isHeaderVisible = true;
+    }
+
+    this.previousScrollY = currentScrollY;
+
+    // requestAnimationFrame kullanarak işlemi optimize et
+    if (!this.ticking) {
+      window.requestAnimationFrame(() => {
+        this.ticking = false;
+      });
+      this.ticking = true;
+    }
   }
 }
